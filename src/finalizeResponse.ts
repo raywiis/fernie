0cecf8d858fe88ada864d7478def7d6759d403fc
RayWiis <raywiis@live.com>
2021-07-10 20:19:57 +0300

    Add returns on finalizing response

--- src/finalizeResponse.ts
+++ src/finalizeResponse.ts
@@ -8,15 +8,18 @@ export default function finalizeResponse(
 	if (!sourceRes) {
 		targetRes.statusCode = 404;
 		targetRes.end();
+		return;
 	} else if (typeof sourceRes === "string") {
 		writeBody(targetRes, sourceRes);
 		targetRes.end();
+		return;
 	} else if (typeof sourceRes === "object") {
 		targetRes.statusCode = sourceRes.statusCode || 200;
 		if (sourceRes.body) {
 			writeBody(targetRes, sourceRes.body);
 		}
 		targetRes.end();
+		return;
 	}
 	throw new Error("Response finalizer not implemented");
 }
